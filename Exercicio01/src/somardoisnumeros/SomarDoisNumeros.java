d7f2046e119d67e65c2a587b32f0b72e58c79992
Matheusfr34 <164033400+Matheusfr34@users.noreply.github.com>
2024-08-13 15:52:25 -0300

    Atualizacao

--- Exercicio01/src/somardoisnumeros/SomarDoisNumeros.java
+++ Exercicio01/src/somardoisnumeros/SomarDoisNumeros.java
@@ -4,16 +4,16 @@ import java.util.*;
 
 class SomarDoisNumeros {
 	
-	public static Scanner sc = new Scanner(System.in);
+	public static Scanner teclado = new Scanner(System.in);
 	
 	public static void main (String args[]){
 		//Declaracao de variaveis
 		int num1, num2, soma;
 		//Leituras
 		System.out.println("Digite um número");
-		num1 = sc.nextInt();
+		num1 = teclado.nextInt();
 		System.out.println("Digite outro número");
-		num2 = sc.nextInt();
+		num2 = teclado.nextInt();
 		//Somar
 		soma = num1 + num2;
 		//Mostrar na tela
